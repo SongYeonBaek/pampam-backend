144a315aeddb8cd4e76c747a5557c79bf361e8e8
SongYeon <songyeon0607@naver.com>
2024-07-17 15:33:35 +0900

    [Refactor] 카트 중복 시 결제 안되는 에러 해결 - findAllBy_ 메소드를 통해 중복된 상품까지 모두 불러와서 하나 제거하도록 변경

--- pampam-service/src/main/java/com/example/pampam/cart/service/CartService.java
+++ pampam-service/src/main/java/com/example/pampam/cart/service/CartService.java
@@ -82,11 +82,19 @@ public class CartService {
 
     //장바구니 삭제 - Order이 진행된 상품 삭제
     public BaseResponse<String> deleteOrderedCart(Long consumerIdx, Long productIdx) {
-        Optional<Cart> cart = cartRepository.findByConsumerIdxAndProductIdx(consumerIdx, productIdx);
-        if(cart.isPresent())
-            cartRepository.deleteById(cart.get().getIdx());
-        return BaseResponse.successResponse("요청 성공", "요청 성공");
+        try {
+            List<Cart> carts = cartRepository.findAllByConsumerIdxAndProductIdx(consumerIdx, productIdx);
+            if (carts.isEmpty()) {
+                return BaseResponse.failResponse(7000, "요청 실패" + " 카트를 찾을 수 없습니다.");
+            }
 
+            cartRepository.deleteById(carts.get(0).getIdx());
+
+
+            return BaseResponse.successResponse("요청 성공", "요청 성공");
+        } catch (Exception e) {
+            return BaseResponse.failResponse(5000, "서버 오류가 발생했습니다.");
+        }
     }
 
 }
