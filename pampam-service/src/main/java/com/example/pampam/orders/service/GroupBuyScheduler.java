b174a596b1b0b750d12fc1bcfa13de16c87a6543
SongYeon <songyeon0607@naver.com>
2024-08-08 18:07:07 +0900

    [Refactor] 스케줄러로 돌아가도록 컨트롤러 제거

--- pampam-service/src/main/java/com/example/pampam/orders/service/GroupBuyScheduler.java
+++ pampam-service/src/main/java/com/example/pampam/orders/service/GroupBuyScheduler.java
@@ -28,7 +28,7 @@ public class GroupBuyScheduler {
     private final PaymentService paymentService;
     private final EmailVerifyService emailVerifyService;
 
-//    @Scheduled(cron="0 5 0 * * *")
+    @Scheduled(cron="0 5 0 * * *")
     public void groupBuy() throws IOException {
         LocalDate now = LocalDate.now(ZoneId.of("Asia/Seoul")).minusDays(1);
         LocalDateTime dateTime = now.atStartOfDay().withHour(9).withMinute(0).withSecond(0).withNano(0);
